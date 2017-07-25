25d6e670c6d37ccbfd68d44f105932cb4fd1f0f2
Kane Valentine <kane@cute.im>
2017-07-25 19:44:54 +0100

    removed some extra tools from the list

--- mybb-sysprep.sh
+++ mybb-sysprep.sh
@@ -6,7 +6,7 @@ fi
 
 export MYBB_VERSION=1811
 export FORUM_DIR="mybb"
-export EXTRA_TOOLS=(apt-transport-https ca-certificates curl git htop nano lsb-release ltrace rsync screenfetch sudo strace unzip wget zsh)
+export EXTRA_TOOLS=(apt-transport-https ca-certificates curl git htop nano lsb-release rsync unzip wget zsh)
 export PHP_BINARIES=(php7.0-bz2 php7.0-curl php7.0-fpm php7.0-gd php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-pgsql php7.0-xml php7.0-zip)
 
 apt-get -y install ${EXTRA_TOOLS[*]}
